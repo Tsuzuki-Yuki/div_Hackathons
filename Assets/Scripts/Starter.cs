3829bf6e359de0b4dec799e54325c435f4a98fda
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2018-08-18 17:41:59 +0900

    float同期の実装

---
+++ Assets/Scripts/Starter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Starter : MonoBehaviour
+{
+	[SerializeField]
+	Button startButton;
+
+	// Use this for initialization
+	void Start ()
+	{
+		startButton.onClick.AddListener(() =>
+		{
+			startButton.gameObject.transform.root.gameObject.SetActive(false);
+			//アニメーション再生
+			//音楽再生
+		});
+	}
+}
